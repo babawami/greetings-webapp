ac160ed87a8b2523c265302c15257c31575bcc8b
Andrew <andrew.monamodi@gmail.com>
2018-08-28 10:18:14 +0200

    added heroku json

--- table.sql
+++ table.sql
@@ -1,1 +1,6 @@
-create table users_greeted(id serial primary key, names text not null, names_counter int not null);
+create table users_greeted
+(
+    id serial primary key,
+    names text not null,
+    names_counter int not null
+);
